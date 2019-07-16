a676ac8fb5262772e3f1dd195fd76bac30d02687
Shoaib <msk4862@gmail.com>
2019-07-16 23:19:37 +0530

    initial commit

---
+++ Assets/Scripts/AugmentedLiveImage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+using UnityEngine;
+using GoogleARCore;
+
+public class AugmentedLiveImage : MonoBehaviour {
+
+	[SerializeField]
+	private VideoClip[] videoClips;
+	private VideoPlayer videoPlayer;
+	public AugmentedImage Image; 
+
+	// Use this for initialization
+	void Start () {
+		videoPlayer = GetComponent<VideoPlayer>();
+		videoPlayer.loopPointReached += OnStop;
+	}
+	
+	private void OnStop(VideoPlayer source) {
+		gameObject.SetActive(false);
+	}
+
+	void Update ()
+	{
+		if (Image == null || Image.TrackingState != TrackingState.Tracking) {
+			return;
+		}
+
+		if (!videoPlayer.isPlaying) {
+			videoPlayer.clip = videoClips[Image.DatabaseIndex];
+			videoPlayer.Play();
+		}
+		transform.localScale = new Vector3(Image.ExtentX, Image.ExtentZ, 1);
+	}
+}
---
+++ Assets/Scripts/LiveImageController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class LiveImageController : MonoBehaviour {
+
+	public AugmentedLiveImage augmentedLiveImage;
+
+	private Dictionary<int, AugmentedLiveImage> visualizers = new Dictionary<int, AugmentedLiveImage>();
+
+	private List<AugmentedImage> images = new List<AugmentedImage>();
+
+	void Start ()
+	{
+		Debug.Log("Starting");
+	}
+
+	void Update ()
+	{
+		if (Session.Status != SessionStatus.Tracking) {
+			return;
+		}
+
+		Session.GetTrackables(images, TrackableQueryFilter.Updated);
+		VisualizeTrackables();
+
+
+	}
+
+
+	private void VisualizeTrackables ()
+	{
+		foreach (var image in images) {
+			Debug.Log("trackable found");
+			var visualizer = GetVisualizer (image);
+
+			if (image.TrackingState == TrackingState.Tracking && visualizer == null) {
+				AddVisualizer (image);
+			} else if(image.TrackingState == TrackingState.Paused && visualizer != null){
+				RemoveVisualizer(image, visualizer);
+			}
+		}
+	}
+
+
+	private void AddVisualizer (AugmentedImage image)
+	{
+		Debug.Log("Adding visualizer");
+		var anchor = image.CreateAnchor(image.CenterPose);
+		var visualizor = Instantiate(augmentedLiveImage, anchor.transform);
+		visualizor.Image = image;
+
+		visualizers.Add(image.DatabaseIndex, visualizor);
+	}
+
+
+	private void RemoveVisualizer (AugmentedImage image, AugmentedLiveImage visualizer)
+	{
+		Debug.Log("Removing visualizer");
+		visualizers.Remove(image.DatabaseIndex);
+		Destroy(visualizer.gameObject);
+	}
+
+		
+	private AugmentedLiveImage GetVisualizer (AugmentedImage image)
+	{
+		AugmentedLiveImage visualizer;
+		visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+		return visualizer;
+	}
+
+}
